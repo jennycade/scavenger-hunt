3b82bedf1963bdf14103fe307e3a888a5130f667
Jenny Zonka <jennycade@gmail.com>
2021-08-30 15:01:02 -0700

    Added mobile warning

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -6,6 +6,7 @@ const StartScreen = ( props ) => {
 
   return (
     <div className="startScreen">
+      <p className="mobileOnly">WARNING: This game has not been designed to play on mobile devices (yet). Go find a laptop and play it there!</p>
       { props.children }
       <button onClick={ startGame }>Start the clock</button>
     </div>
